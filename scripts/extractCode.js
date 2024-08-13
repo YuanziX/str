ecb3131ab1a12cc4d9954f125dc2c0b5e0179475
lqsky7 <catinice@outlook.com>
2024-08-13 21:26:40 +0530

    ash --> monaco
    tuf+ uses a different editor
    *update documentation
    
    Signed-off-by: lqsky7 <catinice@outlook.com>

--- scripts/extractCode.js
+++ scripts/extractCode.js
@@ -1,14 +1,14 @@
 const getCodeScript = `
-  var userSolution = ace.edit("ace-editor").getValue();
+  var userSolution = monaco.editor.getModels()[0].getValue();
   var scriptInjectedElement = document.createElement("pre");
-  scriptInjectedElement.innerText+=userSolution;
-  scriptInjectedElement.setAttribute("id","extractedUserSolution");
-  scriptInjectedElement.setAttribute("style","color:#fff");
+  scriptInjectedElement.innerText += userSolution;
+  scriptInjectedElement.setAttribute("id", "extractedUserSolution");
+  scriptInjectedElement.setAttribute("style", "color:#fff");
   document.body.appendChild(scriptInjectedElement);
-  `;
+`;
 
-  var extractCodeScript = document.createElement('script');
-  extractCodeScript.id = 'extractCodeScript';
-  extractCodeScript.appendChild(document.createTextNode(getCodeScript));
+var extractCodeScript = document.createElement('script');
+extractCodeScript.id = 'extractCodeScript';
+extractCodeScript.appendChild(document.createTextNode(getCodeScript));
 
-  (document.body || document.head || document.documentElement).appendChild(extractCodeScript);
+(document.body || document.head || document.documentElement).appendChild(extractCodeScript);
